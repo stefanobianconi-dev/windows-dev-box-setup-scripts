5002a51f44c8a59f7c335916a7abf82689516111
stefanobianconi-dev <stefano.bianconi@axesstmc.com>
2020-06-10 23:55:25 +0200

    Add install CommonDevTools.ps1

--- dev_app_embedded.ps1
+++ dev_app_embedded.ps1
@@ -26,7 +26,7 @@ executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "DisableServices.ps1"
-# executeScript "CommonDevTools.ps1";
+executeScript "CommonDevTools.ps1";
 
 #--- Tools ---
 #--- Installing VS and VS Code with Git
