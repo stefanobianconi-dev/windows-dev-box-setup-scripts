a79bd00cd7e491e8d2d8061db30f112cd6f259aa
stefanobianconi-dev <stefano.bianconi@axesstmc.com>
2020-06-10 23:10:58 +0200

    Disable unwanted services

--- dev_app_embedded.ps1
+++ dev_app_embedded.ps1
@@ -25,6 +25,7 @@ function executeScript {
 executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
+executeScript "DisableServices.ps1"
 # executeScript "CommonDevTools.ps1";
 
 #--- Tools ---
