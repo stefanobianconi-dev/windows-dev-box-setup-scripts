3b61d3961bea87602591559d7288829507f93cd3
stefanobianconi-dev <stefano.bianconi@axesstmc.com>
2020-06-13 18:40:27 +0200

    Add Office packages

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,10 @@
 
 # tools we expect devs across many scenarios will want
-choco install -y vscode
+choco install -y vscode --params "/NoDesktopIcon"
 choco install -y git.install --params "/GitAndUnixToolsOnPath /WindowsTerminal /NoGuiHereIntegration /SChannel"
+choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System'
+choco install -y tortoisesvn
+choco install -y tortoisegit
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
@@ -14,7 +17,4 @@ choco install -y teraterm
 choco install -y putty.install
 choco install -y curl
 choco install -y mobaxterm
-choco install -y toggl
 choco install -y windirstat
-choco install -y wireshark
-choco install -y angryip
---
+++ scripts/Office.ps1
@@ -0,0 +1,5 @@
+#--- Office ---
+choco install -y microsoft-edge
+choco install -y toggl
+choco install -y openoffice
+choco install -y keepass
